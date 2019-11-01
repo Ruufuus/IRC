5a70649c35fb28fca3eab475b408390ff8718c1e
Ruufuus <45013153+Ruufuus@users.noreply.github.com>
2019-11-01 02:51:13 +0100

    Update threadedServer.c
    -dodanie wysylania i odczytywania odpornego na fragmentacje (tak mysle)
    -dodanie funkcji wykrywajacej komendy
    -dodanie paru komentarzy

--- threadedServer.c
+++ threadedServer.c
@@ -24,52 +24,92 @@ int connection_socket_descriptor;
 int * descriptor_array;
 };
 
+//funkcja wysylajaca wiadomosci z ochrona przeciw fragmentacji
+void bezpieczne_wysylanie(struct thread_data_t * t_data, char * tresc){
+    /*
+    *funkcja w petli wysyla kolejne fragmenty tresci wiadmosci,
+    az nie wysle sumarycznie tyle bajtow, co ma tresc wiadomosci.
+    W kazdej iteracji petli while, w zmiennej temp zapisujemy
+    tymczasowo coraz to mniejsza czesc tresci wiadomosci
+    */
+    int write_result=0;
+    int dlugosc_wiadomosci=sizeof(tresc);
+    char * temp;
+    do{
+        temp = malloc(dlugosc_wiadomosci-write_result);
+        strncpy(temp,tresc+write_result,dlugosc_wiadomosci-write_result);
+        write_result+=write(t_data->connection_socket_descriptor,temp,strlen(temp));
+        free(temp);
+    }while(write_result!=dlugosc_wiadomosci);
+
+}
+
+//funkcja odbierajaca wiadomosci z ochrona przeciw fragmentacji
+char * bezpieczne_odczytywanie(struct thread_data_t * t_data,int * czy_polaczony){
+    /*
+    Funkcja ta w kolejnych iteracjach petli while,
+    odczytuje po jednym znaku, az nie natrafi na znak nowej linii
+    Funkcja dodatkowo wykrywa czy uzytkownik sie nie rozlaczyl
+    */
+    char * buffor = malloc(sizeof(char)*BUFF_SIZE);
+    char * temp = malloc(sizeof(char));
+    int read_result;
+    do{
+        read_result=read(t_data->connection_socket_descriptor,temp,1);
+        if(read_result<0)
+        {
+            printf("Nastapil blad przy odczycie!\n");
+            break;
+        }
+        else if(read_result==0){
+            printf("Uzytkownik sie rozlaczyl!\n");
+            for(int i=0;i<DESCRIPTION_ARRAY_SIZE;i++)
+                {
+                    if(t_data->descriptor_array[i]==t_data->connection_socket_descriptor)
+                        t_data->descriptor_array[i]=-1;
+                        break;
+                }
+                czy_polaczony=0;
+            break;
+        }
+        strcat(buffor,temp);
+    }while(strcmp(temp,"\n"));
+    free(temp);
+    return buffor;
+}
+
+//funkcja wykrywa czy wiadomosc zaczyna sie od ciagu znakow zawartych w komendzie
+int wykrywanie_komendy(char * wiadomosc, char * komenda){
+    return !strncmp(wiadomosc, komenda,strlen(komenda));
+}
+
 //funkcja opisującą zachowanie wątku - musi przyjmować argument typu (void *) i zwracać (void *)
 void *ThreadBehavior(void *t_data)
 {
     pthread_detach(pthread_self());
     int czy_polaczony=1;
     struct thread_data_t *th_data = (struct thread_data_t*)t_data;
-    char * buffor = malloc(sizeof(char)*BUFF_SIZE);
-    //dostęp do pól struktury: (*th_data).pole
-    //TODO (przy zadaniu 1) klawiatura -> wysyłanie albo odbieranie -> wyświetlanie
+    char * buffor;
     while(czy_polaczony)
     {
-        printf("Nasluchuje połączenie o sockecie: %d\n",th_data->connection_socket_descriptor);
-        int read_result=read(th_data->connection_socket_descriptor,buffor,BUFF_SIZE);
-        if(read_result<0){
-            if(read_result==0){
-                printf("Połączenie socketu %d zakończone\n",th_data->connection_socket_descriptor);
-                for(int i=0;i<DESCRIPTION_ARRAY_SIZE;i++)
-                {
-                    if(th_data->descriptor_array[i]==th_data->connection_socket_descriptor)
-                        th_data->descriptor_array[i]=-1;
-                        break;
-                }
-            }
-            else
-            {
-            printf("error przy odczycie!\n");
-            }
+        buffor=bezpieczne_odczytywanie(th_data,&czy_polaczony);
+        if(wykrywanie_komendy(buffor,"$leave"))
+        {
+            czy_polaczony=0;
+            printf("Uzytkownik podlaczony do socketu %d sie rozlaczyl!\n",th_data->connection_socket_descriptor);
         }
         else
-            {
-                if(!strncmp(buffor,"\\leave",strlen("\\leave")) && strlen(buffor)-1==strlen("\\leave"))
+        {
+            printf("Serwer otrzymal wiadomosc o tresci: %s\n",buffor);
+            for(int i=0;i<DESCRIPTION_ARRAY_SIZE;i++)
                 {
-                    czy_polaczony=0;
-                    printf("Uzytkownik podlaczony do socketu %d sie rozlaczyl!\n",th_data->connection_socket_descriptor);
+                if(th_data->descriptor_array[i]!=th_data->connection_socket_descriptor && th_data->descriptor_array[i]!=-1)
+                bezpieczne_wysylanie(th_data,buffor);
                 }
-                else{
-                    printf("Serwer otrzymal wiadomosc o tresci: %s\n",buffor);
-                    for(int i=0;i<DESCRIPTION_ARRAY_SIZE;i++){
-                        if(th_data->descriptor_array[i]!=th_data->connection_socket_descriptor && th_data->descriptor_array[i]!=-1)
-                            write(th_data->descriptor_array[i],buffor,strlen(buffor));
-                    }
-                }
-            }
+        }
+        free(buffor);
     }
     free(th_data);
-    free(buffor);
     pthread_exit(NULL);
 }
 
@@ -80,9 +120,6 @@ void handleConnection(int connection_socket_descriptor,int * descriptor_array) {
 
     //uchwyt na wątek
     pthread_t thread1;
-    //dane, które zostaną przekazane do wątku
-    //TODO dynamiczne utworzenie instancji struktury thread_data_t o nazwie t_data (+ w odpowiednim miejscu zwolnienie pamięci)
-    //TODO wypełnienie pól struktury
     struct thread_data_t* t_data=malloc(sizeof(struct thread_data_t));
     t_data->descriptor_array=descriptor_array;
     t_data->connection_socket_descriptor=connection_socket_descriptor; 
@@ -91,7 +128,6 @@ void handleConnection(int connection_socket_descriptor,int * descriptor_array) {
     printf("Błąd przy próbie utworzenia wątku, kod błędu: %d\n", create_result);
     exit(-1);
     }
-    //TODO (przy zadaniu 1) odbieranie -> wyświetlanie albo klawiatura -> wysyłanie
 }
 
 int main(int argc, char* argv[])
