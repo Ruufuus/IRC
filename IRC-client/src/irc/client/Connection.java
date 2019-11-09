aa4a08a0d1e5da0ddd1be58a988345a24e35fa2c
delrisu <emeryt360@gmail.com>
2019-11-09 20:10:13 +0100

    added connection class

---
+++ IRC-client/src/irc/client/Connection.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package irc.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Robert Ciemny
+ */
+public class Connection implements Runnable{
+    Socket clientSocket = null;
+    
+    
+    
+    public Connection(String ip, int port){
+        try {
+            this.clientSocket = new Socket(ip, port);
+        } catch (IOException ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void run() {
+        
+    }
+    
+    ////////////////////////////////////////////////////PRIVATE CLASSES///////////////////////////////////////////////////////////////////////////
+    
+    private class Reader implements Runnable{
+
+        @Override
+        public void run() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    private class Writer implements Runnable{
+
+        @Override
+        public void run() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+}
